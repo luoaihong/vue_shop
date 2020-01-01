cf88852c4749acf456efb04ec0dfcea32b5bde17
luoaihong <lahemail@163.com>
2020-01-01 18:58:11 +0800

    完成用户列表功能

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求拦截
+axios.interceptors.request.use(config => {
+  config.headers.authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
